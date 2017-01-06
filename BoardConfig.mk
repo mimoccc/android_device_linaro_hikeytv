8269e28a8a44b6a6df7f39a83eab0c54eda6f2ce
Trevor Drake <trevd1234@gmail.com>
2017-01-06 02:26:07 +0000

    Adjust the Boardconfig.mk
    The BOARD_KERNEL_CMDLINE is now inherited and appended to.
    
    Set the lcd denisty=320 in the default.prop this is the density
    of the fugu. doing it in default.prop makes sure we get tp it first

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,11 @@
 include device/linaro/hikey/hikey/BoardConfig.mk
 
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.console=ttyFIQ0 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 #TARGET_NO_RECOVERY := false
 #TARGET_RECOVERY_FSTAB := device/linaro/hikey/fstab.hikey
 
 # Default adb/tcp enabled by default
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    service.adb.tcp.port = 5555
+    service.adb.tcp.port = 5555 \
+    ro.sf.lcd_density = 320
